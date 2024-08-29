0c4a252744777d0f3adf5ef4c46cfbe78d41b3cd
Naveen <naveenkrishnawodeyar@gmail.com>
2024-08-29 08:37:04 +0530

    initial commit, sb_master'

---
+++ sb_master/src/main/java/com/spring/master/SbMasterApplication.java
@@ -0,0 +1,13 @@
+package com.spring.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMasterApplication.class, args);
+	}
+
+}
